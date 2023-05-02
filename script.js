071c0650296cd37a437660431c3a69f4e937c967
Christian Lee <109066855+christianleedev@users.noreply.github.com>
2023-05-02 17:54:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-fetch('https://zenquotes.io/api/today')
+/*fetch('https://animechan.vercel.app/api/random')
     .then(res => {
         return res.json();
     })
     .then(data => {
-        data.forEach(quote => {
-            const markup = `<div class='quote'>"${quote.q}"</div>`;
+        data.forEach(anime => {
+            const markup = `<div class='quote'>"${anime.quote}"</div>`;
             document.querySelector('h1').insertAdjacentHTML('beforeend', markup);
-            const markup2 = `<div class='author'>- ${quote.a}</div`;
+            const markup2 = `<div class='author'>- ${anime.character}</div`;
             document.querySelector('h1').insertAdjacentHTML('beforeend', markup2);
         });
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error));*/
+
+fetch("https://animechan.vercel.app/api/random")
+    .then((response) => response.json())
+    .then((anime) => {
+        const markup = `<div class='quote'>"${anime.quote}"</div>`;
+        document.querySelector('h1').insertAdjacentHTML('beforeend', markup);
+        const markup2 = `<div class='author'>- ${anime.character}</div`;
+        document.querySelector('h1').insertAdjacentHTML('beforeend', markup2);
+        const markup3 = `<div class='anime'>${anime.anime}</div`;
+        document.querySelector('h1').insertAdjacentHTML('beforeend', markup3);
+    });
