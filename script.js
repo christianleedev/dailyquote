ba6d42027b44bcefb65b6400d166d314110dfcf2
Christian Lee <109066855+christianleedev@users.noreply.github.com>
2023-05-03 15:50:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -12,13 +12,35 @@
     })
     .catch(error => console.log(error));*/
 
-fetch("https://animechan.vercel.app/api/random")
+/*fetch("https://quote-garden.onrender.com/api/v3/quotes/random?genre=motivational")
     .then((response) => response.json())
-    .then((anime) => {
-        const markup = `<div class='quote'>"${anime.quote}"</div>`;
+    .then((quote) => {
+        const markup = `<div class='quote'>"${quote.data[0].quoteText}"</div>`;
         document.querySelector('h1').insertAdjacentHTML('beforeend', markup);
-        const markup2 = `<div class='author'>- ${anime.character}</div`;
+        const markup2 = `<div class='author'>- ${quote.data[0].quoteAuthor}</div`;
         document.querySelector('h1').insertAdjacentHTML('beforeend', markup2);
-        const markup3 = `<div class='anime'>${anime.anime}</div`;
-        document.querySelector('h1').insertAdjacentHTML('beforeend', markup3);
+    });*/
+
+    fetch("https://quote-garden.onrender.com/api/v3/quotes/random?genre=motivational")
+    .then((response) => response.json())
+    .then((quote) => {
+        const quoteText = quote.data[0].quoteText;
+        const quoteAuthor = quote.data[0].quoteAuthor;
+    
+        const quoteElement = document.createElement('div');
+        quoteElement.classList.add('quote', 'fade-in');
+        quoteElement.textContent = `"${quoteText}"`;
+    
+        const authorElement = document.createElement('div');
+        authorElement.classList.add('author', 'fade-in');
+        authorElement.textContent = `- ${quoteAuthor}`;
+    
+        const containerElement = document.querySelector('h1');
+        containerElement.appendChild(quoteElement);
+        containerElement.appendChild(authorElement);
+
+        setTimeout(() => {
+            quoteElement.classList.add('fade-in');
+            authorElement.classList.add('fade-in');
+        }, 100);
     });
